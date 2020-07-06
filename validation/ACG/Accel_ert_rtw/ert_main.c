44daa2461c96c04d5dac89749de6661f6c4480d9
AleLuppi <alelup@yahoo.it>
2020-07-06 22:52:14 +0200

    Code generation and PIL validation
    Automatic code generation for STM32F4-Discovery. Creation of a PIL block for the controller subsystem and creation of a car model integrated with PIL block of the controller.

---
+++ validation/ACG/Accel_ert_rtw/ert_main.c
@@ -0,0 +1,101 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'Accel'.
+ *
+ * Model version                  : 1.101
+ * Simulink Coder version         : 9.2 (R2019b) 18-Jul-2019
+ * C/C++ source code generated on : Mon Jul  6 22:04:55 2020
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "Accel.h"
+#include "rtwtypes.h"
+
+volatile int IsrOverrun = 0;
+static boolean_T OverrunFlag = 0;
+void rt_OneStep(void)
+{
+  /* Check for overrun. Protect OverrunFlag against preemption */
+  if (OverrunFlag++) {
+    IsrOverrun = 1;
+    OverrunFlag--;
+    return;
+  }
+
+  __enable_irq();
+  Accel_step();
+
+  /* Get model outputs here */
+  __disable_irq();
+  OverrunFlag--;
+}
+
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = false;
+int main(int argc, char **argv)
+{
+  float modelBaseRate = 0.1;
+  float systemClock = 168;
+
+  /* Initialize variables */
+  stopRequested = false;
+  runModel = false;
+
+#ifndef USE_RTX
+#if defined(MW_MULTI_TASKING_MODE) && (MW_MULTI_TASKING_MODE == 1)
+
+  MW_ASM (" SVC #1");
+
+#endif
+
+  __disable_irq();
+
+#endif
+
+  ;
+  stm32f4xx_init_board();
+  SystemCoreClockUpdate();
+  bootloaderInit();
+  rtmSetErrorStatus(Accel_M, 0);
+  Accel_initialize();
+  ARMCM_SysTick_Config(modelBaseRate);
+  runModel =
+    rtmGetErrorStatus(Accel_M) == (NULL);
+  __enable_irq();
+  __enable_irq();
+  while (runModel) {
+    stopRequested = !(
+                      rtmGetErrorStatus(Accel_M) == (NULL));
+    runModel = !(stopRequested);
+  }
+
+  /* Disable rt_OneStep() here */
+
+  /* Terminate model */
+  Accel_terminate();
+
+#ifndef USE_RTX
+
+  (void)systemClock;
+
+#endif
+
+  ;
+  __disable_irq();
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ validation/ACG/Accel_ert_rtw/html/define.js
@@ -0,0 +1,133 @@
+function CodeDefine() { 
+this.def = new Array();
+this.def["IsrOverrun"] = {file: "ert_main_c.html",line:23,type:"var"};
+this.def["ert_main.c:OverrunFlag"] = {file: "ert_main_c.html",line:24,type:"var"};
+this.def["rt_OneStep"] = {file: "ert_main_c.html",line:25,type:"fcn"};
+this.def["stopRequested"] = {file: "ert_main_c.html",line:42,type:"var"};
+this.def["runModel"] = {file: "ert_main_c.html",line:43,type:"var"};
+this.def["main"] = {file: "ert_main_c.html",line:44,type:"fcn"};
+this.def["Accel_B"] = {file: "Accel_c.html",line:24,type:"var"};
+this.def["Accel_DW"] = {file: "Accel_c.html",line:27,type:"var"};
+this.def["Accel_U"] = {file: "Accel_c.html",line:30,type:"var"};
+this.def["Accel_Y"] = {file: "Accel_c.html",line:33,type:"var"};
+this.def["Accel_M_"] = {file: "Accel_c.html",line:36,type:"var"};
+this.def["Accel_M"] = {file: "Accel_c.html",line:37,type:"var"};
+this.def["look1_binlcapw"] = {file: "Accel_c.html",line:38,type:"fcn"};
+this.def["rt_powd_snf"] = {file: "Accel_c.html",line:100,type:"fcn"};
+this.def["rt_hypotd_snf"] = {file: "Accel_c.html",line:146,type:"fcn"};
+this.def["rt_atan2d_snf"] = {file: "Accel_c.html",line:167,type:"fcn"};
+this.def["Accel_step"] = {file: "Accel_c.html",line:204,type:"fcn"};
+this.def["Accel_initialize"] = {file: "Accel_c.html",line:1070,type:"fcn"};
+this.def["Accel_terminate"] = {file: "Accel_c.html",line:1085,type:"fcn"};
+this.def["B_Accel_T"] = {file: "Accel_h.html",line:57,type:"type"};
+this.def["DW_Accel_T"] = {file: "Accel_h.html",line:62,type:"type"};
+this.def["ExtU_Accel_T"] = {file: "Accel_h.html",line:72,type:"type"};
+this.def["ExtY_Accel_T"] = {file: "Accel_h.html",line:77,type:"type"};
+this.def["struct_qm0PExI0AxHWASzOnN0CqB"] = {file: "Accel_types_h.html",line:52,type:"type"};
+this.def["P_Accel_T"] = {file: "Accel_types_h.html",line:57,type:"type"};
+this.def["RT_MODEL_Accel_T"] = {file: "Accel_types_h.html",line:60,type:"type"};
+this.def["Accel_P"] = {file: "Accel_data_c.html",line:24,type:"var"};
+this.def["rtGetInf"] = {file: "rtGetInf_c.html",line:31,type:"fcn"};
+this.def["rtGetInfF"] = {file: "rtGetInf_c.html",line:55,type:"fcn"};
+this.def["rtGetMinusInf"] = {file: "rtGetInf_c.html",line:66,type:"fcn"};
+this.def["rtGetMinusInfF"] = {file: "rtGetInf_c.html",line:90,type:"fcn"};
+this.def["rtGetNaN"] = {file: "rtGetNaN_c.html",line:31,type:"fcn"};
+this.def["rtGetNaNF"] = {file: "rtGetNaN_c.html",line:55,type:"fcn"};
+this.def["rtInf"] = {file: "rt_nonfinite_c.html",line:30,type:"var"};
+this.def["rtMinusInf"] = {file: "rt_nonfinite_c.html",line:31,type:"var"};
+this.def["rtNaN"] = {file: "rt_nonfinite_c.html",line:32,type:"var"};
+this.def["rtInfF"] = {file: "rt_nonfinite_c.html",line:33,type:"var"};
+this.def["rtMinusInfF"] = {file: "rt_nonfinite_c.html",line:34,type:"var"};
+this.def["rtNaNF"] = {file: "rt_nonfinite_c.html",line:35,type:"var"};
+this.def["rt_InitInfAndNaN"] = {file: "rt_nonfinite_c.html",line:41,type:"fcn"};
+this.def["rtIsInf"] = {file: "rt_nonfinite_c.html",line:53,type:"fcn"};
+this.def["rtIsInfF"] = {file: "rt_nonfinite_c.html",line:59,type:"fcn"};
+this.def["rtIsNaN"] = {file: "rt_nonfinite_c.html",line:65,type:"fcn"};
+this.def["rtIsNaNF"] = {file: "rt_nonfinite_c.html",line:87,type:"fcn"};
+this.def["BigEndianIEEEDouble"] = {file: "rt_nonfinite_h.html",line:41,type:"type"};
+this.def["LittleEndianIEEEDouble"] = {file: "rt_nonfinite_h.html",line:48,type:"type"};
+this.def["IEEESingle"] = {file: "rt_nonfinite_h.html",line:55,type:"type"};
+this.def["int8_T"] = {file: "rtwtypes_h.html",line:51,type:"type"};
+this.def["uint8_T"] = {file: "rtwtypes_h.html",line:52,type:"type"};
+this.def["int16_T"] = {file: "rtwtypes_h.html",line:53,type:"type"};
+this.def["uint16_T"] = {file: "rtwtypes_h.html",line:54,type:"type"};
+this.def["int32_T"] = {file: "rtwtypes_h.html",line:55,type:"type"};
+this.def["uint32_T"] = {file: "rtwtypes_h.html",line:56,type:"type"};
+this.def["real32_T"] = {file: "rtwtypes_h.html",line:57,type:"type"};
+this.def["real64_T"] = {file: "rtwtypes_h.html",line:58,type:"type"};
+this.def["real_T"] = {file: "rtwtypes_h.html",line:64,type:"type"};
+this.def["time_T"] = {file: "rtwtypes_h.html",line:65,type:"type"};
+this.def["boolean_T"] = {file: "rtwtypes_h.html",line:66,type:"type"};
+this.def["int_T"] = {file: "rtwtypes_h.html",line:67,type:"type"};
+this.def["uint_T"] = {file: "rtwtypes_h.html",line:68,type:"type"};
+this.def["ulong_T"] = {file: "rtwtypes_h.html",line:69,type:"type"};
+this.def["char_T"] = {file: "rtwtypes_h.html",line:70,type:"type"};
+this.def["uchar_T"] = {file: "rtwtypes_h.html",line:71,type:"type"};
+this.def["byte_T"] = {file: "rtwtypes_h.html",line:72,type:"type"};
+this.def["creal32_T"] = {file: "rtwtypes_h.html",line:82,type:"type"};
+this.def["creal64_T"] = {file: "rtwtypes_h.html",line:87,type:"type"};
+this.def["creal_T"] = {file: "rtwtypes_h.html",line:92,type:"type"};
+this.def["cint8_T"] = {file: "rtwtypes_h.html",line:99,type:"type"};
+this.def["cuint8_T"] = {file: "rtwtypes_h.html",line:106,type:"type"};
+this.def["cint16_T"] = {file: "rtwtypes_h.html",line:113,type:"type"};
+this.def["cuint16_T"] = {file: "rtwtypes_h.html",line:120,type:"type"};
+this.def["cint32_T"] = {file: "rtwtypes_h.html",line:127,type:"type"};
+this.def["cuint32_T"] = {file: "rtwtypes_h.html",line:134,type:"type"};
+this.def["pointer_T"] = {file: "rtwtypes_h.html",line:152,type:"type"};
+}
+CodeDefine.instance = new CodeDefine();
+var testHarnessInfo = {OwnerFileName: "", HarnessOwner: "", HarnessName: "", IsTestHarness: "0"};
+var relPathToBuildDir = "../ert_main.c";
+var fileSep = "\\";
+var isPC = true;
+function Html2SrcLink() {
+	this.html2SrcPath = new Array;
+	this.html2Root = new Array;
+	this.html2SrcPath["ert_main_c.html"] = "../ert_main.c";
+	this.html2Root["ert_main_c.html"] = "ert_main_c.html";
+	this.html2SrcPath["Accel_c.html"] = "../Accel.c";
+	this.html2Root["Accel_c.html"] = "Accel_c.html";
+	this.html2SrcPath["Accel_h.html"] = "../Accel.h";
+	this.html2Root["Accel_h.html"] = "Accel_h.html";
+	this.html2SrcPath["Accel_private_h.html"] = "../Accel_private.h";
+	this.html2Root["Accel_private_h.html"] = "Accel_private_h.html";
+	this.html2SrcPath["Accel_types_h.html"] = "../Accel_types.h";
+	this.html2Root["Accel_types_h.html"] = "Accel_types_h.html";
+	this.html2SrcPath["Accel_data_c.html"] = "../Accel_data.c";
+	this.html2Root["Accel_data_c.html"] = "Accel_data_c.html";
+	this.html2SrcPath["rtGetInf_c.html"] = "../rtGetInf.c";
+	this.html2Root["rtGetInf_c.html"] = "rtGetInf_c.html";
+	this.html2SrcPath["rtGetInf_h.html"] = "../rtGetInf.h";
+	this.html2Root["rtGetInf_h.html"] = "rtGetInf_h.html";
+	this.html2SrcPath["rtGetNaN_c.html"] = "../rtGetNaN.c";
+	this.html2Root["rtGetNaN_c.html"] = "rtGetNaN_c.html";
+	this.html2SrcPath["rtGetNaN_h.html"] = "../rtGetNaN.h";
+	this.html2Root["rtGetNaN_h.html"] = "rtGetNaN_h.html";
+	this.html2SrcPath["rt_defines_h.html"] = "../rt_defines.h";
+	this.html2Root["rt_defines_h.html"] = "rt_defines_h.html";
+	this.html2SrcPath["rt_nonfinite_c.html"] = "../rt_nonfinite.c";
+	this.html2Root["rt_nonfinite_c.html"] = "rt_nonfinite_c.html";
+	this.html2SrcPath["rt_nonfinite_h.html"] = "../rt_nonfinite.h";
+	this.html2Root["rt_nonfinite_h.html"] = "rt_nonfinite_h.html";
+	this.html2SrcPath["rtwtypes_h.html"] = "../rtwtypes.h";
+	this.html2Root["rtwtypes_h.html"] = "rtwtypes_h.html";
+	this.html2SrcPath["rtmodel_h.html"] = "../rtmodel.h";
+	this.html2Root["rtmodel_h.html"] = "rtmodel_h.html";
+	this.html2SrcPath["MW_target_hardware_resources_h.html"] = "../MW_target_hardware_resources.h";
+	this.html2Root["MW_target_hardware_resources_h.html"] = "MW_target_hardware_resources_h.html";
+	this.getLink2Src = function (htmlFileName) {
+		 if (this.html2SrcPath[htmlFileName])
+			 return this.html2SrcPath[htmlFileName];
+		 else
+			 return null;
+	}
+	this.getLinkFromRoot = function (htmlFileName) {
+		 if (this.html2Root[htmlFileName])
+			 return this.html2Root[htmlFileName];
+		 else
+			 return null;
+	}
+}
+Html2SrcLink.instance = new Html2SrcLink();
+var fileList = [
+"ert_main_c.html","Accel_c.html","Accel_h.html","Accel_private_h.html","Accel_types_h.html","Accel_data_c.html","rtGetInf_c.html","rtGetInf_h.html","rtGetNaN_c.html","rtGetNaN_h.html","rt_defines_h.html","rt_nonfinite_c.html","rt_nonfinite_h.html","rtwtypes_h.html","rtmodel_h.html","MW_target_hardware_resources_h.html"];
